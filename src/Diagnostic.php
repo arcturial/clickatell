4abe0228653559a45584b80cbf9a3161b02a6546
Jonadabe <jonadabe@hotmail.com>
2016-02-13 10:26:59 +0000

    Update Diagnostic.php

--- src/Diagnostic.php
+++ src/Diagnostic.php
@@ -28,18 +28,18 @@ class Diagnostic
      * @var array
      */
     private static $statusCodes = array(
-        "001" => "The message ID is incorrect or reporting is delayed.",
-        "002" => "The message could not be delivered and has been queued for attempted redelivery.",
-        "003" => "Delivered to the upstream gateway or network (delivered to the recipient).",
-        "004" => "Confirmation of receipt on the handset of the recipient.",
-        "005" => "There was an error with the message, probably caused by the content of the message itself.",
-        "006" => "The message was terminated by a user (stop message command) or by our staff.",
-        "007" => "An error occurred delivering the message to the handset. 008 0x008 OK Message received by gateway.",
-        "009" => "The routing gateway or network has had an error routing the message.",
-        "010" => "Message has expired before we were able to deliver it to the upstream gateway. No charge applies.",
-        "011" => "Message has been queued at the gateway for delivery at a later time (delayed delivery).",
-        "012" => "The message cannot be delivered due to a lack of funds in your account. Please re-purchase credits.",
-        "014" => "Maximum MT limit exceeded The allowable amount for MT messaging has been exceeded.",
+        "001" => "Message unknown: The message ID is incorrect or reporting is delayed.",
+        "002" => "Message queued: The message could not be delivered and has been queued for attempted redelivery.",
+        "003" => "Delivered to gateway: Delivered to the upstream gateway or network (delivered to the recipient).",
+        "004" => "Received by recipient: Confirmation of receipt on the handset of the recipient.",
+        "005" => "Error with message: There was an error with the message, probably caused by the content of the message itself.",
+        "006" => "User cancelled message delivery: The message was terminated by a user (stop message command) or by our staff.",
+        "007" => "Error delivering message: An error occurred delivering the message to the handset.",
+        "008" => "OK: Message received by gateway.",
+        "009" => "Routing error: An error occurred while attempting to route the message.",
+        "010" => "Message expired: Message has expired before we were able to deliver it to the upstream gateway. No charge applies.",
+        "011" => "Message queued for later delivery: Message has been queued at the gateway for delivery at a later time (delayed delivery).",
+        "012" => "Out of credit: The message cannot be delivered due to a lack of funds in your account. Please re-purchase credits.",
     );
 
     /**
